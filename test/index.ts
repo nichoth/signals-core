7a7df260dbee97161cac485fb48b03409d21a1e8
nick thomas <nichoth@gmail.com>
2023-10-01 20:39:27 -0700

    smoke tests pass

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,33 @@
 import { test } from '@socketsupply/tapzero'
+import { signal, effect, computed } from '../src/index.js'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+test('signal + subscription', async t => {
+    t.plan(2)
+    const counter = signal(0)
+    let count = 0
+
+    effect(() => {
+        const n = counter.value
+        t.equal(n, count, 'should get the initial value')
+        count++
+    })
+
+    counter.value = 1
+
+    // need this so test doesn't end before all our assertions
+    return Promise.resolve()
+})
+
+test('signal + computed', async t => {
+    const counter = signal(1)
+
+    const x2 = computed(() => {
+        return (counter.value * 2)
+    })
+
+    counter.value = 2
+
+    effect(() => {
+        t.equal(x2.value, 4)
+    })
 })
